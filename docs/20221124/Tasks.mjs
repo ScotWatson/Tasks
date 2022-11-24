2e2e5fad7650bc9e223176eb607b31ebdd16ab58
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-11-24 12:47:58 -0500

    Update Tasks.mjs

--- docs/20221124/Tasks.mjs
+++ docs/20221124/Tasks.mjs
@@ -3,8 +3,8 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-import * as Types from "https://scotwatson.github.io/Debug/Test/Types.mjs";
-import * as ErrorLog from "https://scotwatson.github.io/Debug/Test/ErrorLog.mjs";
+import * as Types from "https://scotwatson.github.io/Debug/20221107/Types.mjs";
+import * as ErrorLog from "https://scotwatson.github.io/Debug/20221107/ErrorLog.mjs";
 
 export function queueTask(taskFunction, args) {
   try {
